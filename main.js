e94bf8cc806cc400786b9da96836439c6959f366
Resized <shai.yarden@gmail.com>
2021-04-07 16:30:23 +0300

    Added background

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ function startGame() {
     myGamePiece = new component(20, 20, "red", 10, 120, "game_piece");
     myGamePiece.gravity = 0.15;
     myScore = new component("30px", "Consolas", "black", canvas_width / 2, 40, "text");
+    myBackground.start();
     myGameArea.start();
 }
 
@@ -24,12 +25,18 @@ function restartGame() {
     myObstacles = [];
 }
 
+var imageRepository = new function () {
+    this.background = new Image();
+    this.background.src = "images/background.png";
+    this.gamePieceImg = new Image();
+    this.gamePieceImg.src = "images/flappy.png";
+    this.ground = new Image();
+    this.ground.src = "images/ground.png";
+}
+
 var myGameArea = {
-    canvas: document.createElement("canvas"),
+    canvas: document.getElementById("game_canvas"),
     start: function () {
-        this.gamePieceImg = document.getElementById("flappy");
-        this.canvas.width = canvas_width;
-        this.canvas.height = canvas_height;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.frameNo = 0;
@@ -37,6 +44,15 @@ var myGameArea = {
     },
     clear: function () {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
+
+var myBackground = {
+    canvas: document.getElementById("background_canvas"),
+    start: function () {
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.context.drawImage(imageRepository.background, 0, 0);
     }
 }
 
@@ -52,9 +68,12 @@ function component(width, height, color, x, y, type) {
     this.gravity = 0;
     this.gravitySpeed = 0;
     this.rotation_angle = 30;
+    this.floor_height = imageRepository.ground.height - 50;
 
     this.update = function () {
         ctx = myGameArea.context;
+        ctx.drawImage(imageRepository.ground, 0, canvas_height - this.floor_height, canvas_width, imageRepository.ground.height);
+
         switch (this.type) {
             case "text":
                 ctx.textAlign = "center";
@@ -67,12 +86,12 @@ function component(width, height, color, x, y, type) {
                 break;
             case "game_piece":
                 ctx.fillStyle = color;
-                this.drawImgRot(myGameArea.gamePieceImg, this.x, this.y, this.width, this.height, this.gravitySpeed * 5);
-                //ctx.drawImage(myGameArea.gamePieceImg, 10, 10);
-                //this.drawRectRot(this.x, this.y, this.width, this.height, this.gravitySpeed * 5);
+                this.drawImgRot(imageRepository.gamePieceImg, this.x - 5, this.y - 5, this.width + 10, this.height + 10, this.gravitySpeed * 5);
                 break;
             case "obstacle_top":
             case "obstacle_bottom":
+                ctx.fillStyle = "black";
+                ctx.fillRect(this.x - 2, this.y - 2, this.width + 4, this.height + 4);
                 var my_gradient = ctx.createLinearGradient(this.x - 20, this.y, this.x + this.width + 10, this.y);
                 my_gradient.addColorStop(0, "black");
                 my_gradient.addColorStop(0.5, color);
@@ -85,50 +104,47 @@ function component(width, height, color, x, y, type) {
                 my_gradient.addColorStop(1, "white");
                 ctx.fillStyle = my_gradient;
                 if (this.type == "obstacle_top") {
+                    ctx.fillStyle = "black";
+                    ctx.fillRect(this.x - 7, this.y + this.height - 12, this.width + 14, 14);
+                    ctx.fillStyle = my_gradient;
+
                     ctx.fillRect(this.x - 5, this.y + this.height - 10, this.width + 10, 10);
                     ctx.fillStyle = 'rgba(255,255,255,0.2)';
                     ctx.fillRect(this.x - 5, this.y + this.height - 10, this.width + 10, 3);
 
                 } else if (this.type == "obstacle_bottom") {
+                    ctx.fillStyle = "black";
+                    ctx.fillRect(this.x - 7, this.y - 2, this.width + 14, 14);
+                    ctx.fillStyle = my_gradient;
+
                     ctx.fillRect(this.x - 5, this.y, this.width + 10, 10);
                     ctx.fillStyle = 'rgba(255,255,255,0.2)';
                     ctx.fillRect(this.x - 5, this.y, this.width + 10, 3);
                 }
-                ctx.fillRect
                 break;
 
             default:
+
                 break;
+
         }
 
     }
     this.drawImgRot = function (img, x, y, width, height, deg) {
-        // Store the current context state (i.e. rotation, translation etc..)
         ctx.save()
-        //Convert degrees to radian 
         var rad = deg * Math.PI / 180;
-        //Set the origin to the center of the image
         ctx.translate(x + width / 2, y + height / 2);
-        //Rotate the canvas around the origin
         ctx.rotate(rad);
-        //draw the image    
         ctx.drawImage(img, width / 2 * (-1), height / 2 * (-1), width, height);
-        // Restore canvas state as saved from above
         ctx.restore();
     }
 
     this.drawRectRot = function (x, y, width, height, deg) {
-        // Store the current context state (i.e. rotation, translation etc..)
         ctx.save()
-        //Convert degrees to radian 
         var rad = deg * Math.PI / 180;
-        //Set the origin to the center of the image
         ctx.translate(x + width / 2, y + height / 2);
-        //Rotate the canvas around the origin
         ctx.rotate(rad);
-        //draw the image    
         ctx.fillRect(width / 2 * (-1), height / 2 * (-1), width, height);
-        // Restore canvas state as saved from above
         ctx.restore();
     }
 
@@ -145,10 +161,10 @@ function component(width, height, color, x, y, type) {
         this.gravitySpeed = 0;
     }
     this.hitBottomTop = function () {
-        var rockbottom = myGameArea.canvas.height - this.height;
+        var rockbottom = myGameArea.canvas.height - this.height - this.floor_height;
         if (this.y > rockbottom) {
-            this.y = rockbottom;
-            this.gravitySpeed = 0;
+            saveHighscore();
+            restartGame();
         }
         if (this.y < 0) {
             this.y = 0;
@@ -180,7 +196,6 @@ function saveHighscore() {
 }
 
 function updateGameArea() {
-    console.log(myGamePiece.speedX);
     var x, height, gap, minHeight, maxHeight, minGap, maxGap;
     var obstacleWidth = 40;
     for (i = 0; i < myObstacles.length; i += 1) {
@@ -204,7 +219,6 @@ function updateGameArea() {
         myObstacles.push(new component(obstacleWidth, height, "green", x, 0, "obstacle_top"));
         myObstacles.push(new component(obstacleWidth, x - height - gap, "green", x, height + gap, "obstacle_bottom"));
     } else if (everyinterval(150)) {
-
         obstacleSpeed += 0.1;
     }
 
